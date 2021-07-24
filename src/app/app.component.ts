c645e5ed2083229f593af163787409263f38acb1
taro04 <75844498+taro04@users.noreply.github.com>
2021-07-24 21:35:29 +0900

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,36 @@ import { cosmosclient, rest, proto } from 'cosmos-client';
 export class AppComponent {
   title = 'cosmos-ts-sdk2';
 
-  sdk = new cosmosclient.CosmosSDK('http://localhost:1317', 'testchain');
+
+  constructor(
+  ) {
+    this.example().then(); //実行
+  }
+
+  public example = async (): Promise<void> => {
+    const sdk = new cosmosclient.CosmosSDK('http://localhost:1317', 'mars')
+    const privKey = new proto.cosmos.crypto.secp256k1.PrivKey({
+        key: await cosmosclient.generatePrivKeyFromMnemonic('joke door law post fragile cruel torch silver siren mechanic flush surround'),
+      });
+    console.log("privkey is ",privKey)
+    const pubKey = privKey.pubKey();
+    console.log("pubkey is ",pubKey)
+    const address = cosmosclient.AccAddress.fromPublicKey(pubKey);
+    console.log("address is ",address)
+    const fromAddress = address;
+    const toAddress = address;
+
+    // get account info
+    const account = await rest.cosmos.auth
+    .account(sdk, fromAddress)
+    .then((res) => res.data.account && cosmosclient.codec.unpackCosmosAny(res.data.account))
+    .catch((_) => undefined);
+    if (!(account instanceof proto.cosmos.auth.v1beta1.BaseAccount)) {
+      console.log("if accout",account);
+      return;
+    }
+  }
+
 
 
 
